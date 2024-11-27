1a6921dd3a1967719bf70f92a794c3daaf2acb76
peslun <p.houska@gmail.com>
2024-11-27 18:39:11 +0100

    package documentation

---
+++ R/_PACKAGE.R
@@ -0,0 +1,14 @@
+#' Rpkg Exam Package
+#'
+#' This package installs function Rgreyscale and Rsplit to be used by the user. See their description.
+#'
+#' @name Rpkg.exam
+#' @import magick
+#' @description
+#' Use this function
+#'   apply_filter("samples/Test image-1.jpg","grayscale")
+#'   apply_filter("samples/Test image-1.jpg","cutoff") #defaults to cutoff 127
+#'   apply_filter("samples/Test image-1.jpg","cutoff",20)
+#'
+
+NULL
